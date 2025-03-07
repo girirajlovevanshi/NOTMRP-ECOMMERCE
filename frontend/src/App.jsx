26e1eaf7eaea96dcca81963900e5db5d1b54abee
Giriraj Lovevanshi <girirajlovewanshi@gmail.com>
2025-03-07 12:44:14 +0530

    home to Home , typo error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import Home from './pages/home'
+import Home from './pages/Home'
 
 function App() {
   return (
